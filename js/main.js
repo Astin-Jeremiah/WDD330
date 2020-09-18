5e82592c2ee77b6bc2e7c13da061fcadf1c72048
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-09-18 00:50:45 -0400

    9/18

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const links =[
+    {
+        label: "Week 1",
+        url: "week1/index.html"
+    },
+          
+    {
+        label: "Week 2",
+        url: "week2/index.html"
+    }
+]
+
+const list = docuemnts.getElementById("weeklist");
+
+for (let i=0; i<links.length; i++)
+    {
+        let litsitems = document.createElement("li");
+        let anchor = document.createElement("a");
+        
+        anchor.setAttribute("href", links[i].url);
+        anchor.innerText = links[i].label;
+        
+        listitems.appendChild(anchor);
+        list.appendChild(listitems);
+    }
