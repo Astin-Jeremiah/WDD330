594a0e0f5c2abe668c3c6da37e7a410045faa37b
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-09-19 01:40:44 -0400

    9-19-20

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 const links =[
-    {
-        label: "Week 1",
-        url: "week1/index.html"
-    },
-          
-    {
-        label: "Week 2",
-        url: "week2/index.html"
-    }
+    { label: "WEEK 1", url: "week1/index.html"},
+    { label: "WEEK 2", url: "week2/index.html"},
+    { label: "WEEK 3", url: "week3/index.html"},
+    { label: "WEEK 4", url: "week4/index.html"},
+    { label: "WEEK 5", url: "week5/index.html"},
+    { label: "WEEK 6", url: "week6/index.html"}
 ]
 
 const list = document.getElementById("weeklist");
