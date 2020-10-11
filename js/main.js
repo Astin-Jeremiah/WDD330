1a7a8e766834b702bfa7aba3ace37385e24d1053
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-10-10 22:42:11 -0400

    10-10 Home Page Changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 const links =[
-    { label: "WEEK 1", url: "week1/index.html"},
-    { label: "WEEK 2", url: "week2/index.html"},
-    { label: "WEEK 3", url: "week3/index.html"},
-    { label: "WEEK 4", url: "week4/index.html"}
+    { image: "images/week1.png", url: "week1/index.html"},
+    { image: "images/week2.png", url: "week2/index.html"},
+    { image: "images/week3.png", url: "week3/index.html"},
+    { image: "images/week4.png", url: "week4/index.html"}, 
+    { image: "images/week5.png", url: "week5/index.html"},
+    { image: "images/week6.png", url: "week6/index.html"}, 
+    { image: "images/week7.png", url: "week7/index.html"}, 
+    { image: "images/week8.png", url: "week8/index.html"}, 
+    { image: "images/week9.png", url: "week9/index.html"}, 
+    { image: "images/week10.png", url: "week10/index.html"}, 
+    { image: "images/week11.png", url: "week11/index.html"},
+    { image: "images/week12.png", url: "week12/index.html"}, 
+    { image: "images/week13.png", url: "week13/index.html"}, 
+    
 ]
 
 const list = document.getElementById("weeklist");
@@ -11,10 +21,17 @@ for (let i=0; i<links.length; i++)
     {
         let listitems = document.createElement("li");
         let anchor = document.createElement("a");
+        let img = document.createElement("img");
+
         
         anchor.setAttribute("href", links[i].url);
-        anchor.innerText = links[i].label;
+        img.setAttribute("src", links[i].image);
+
+        
         
         listitems.appendChild(anchor);
+        anchor.appendChild(img);
         list.appendChild(listitems);
     }
+
+        
