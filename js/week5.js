98822ac185f76354b12e15390333e2baec68014c
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-10-16 17:39:30 -0400

    10-16

---
+++ js/week5.js
@@ -0,0 +1,24 @@
+const links =[
+    { image: "../images/forms.png", url: "testingnotes.html"},
+    { image: "../images/object.png", url: "hikes.html"},
+]
+
+const list = document.getElementById("week5");
+
+for (let i=0; i<links.length; i++)
+    {
+        let listitems = document.createElement("li");
+        let anchor = document.createElement("a");
+        let img = document.createElement("img");
+
+        
+        anchor.setAttribute("href", links[i].url);
+        img.setAttribute("src", links[i].image);
+        img.setAttribute("class", "pressed");
+
+        
+        
+        listitems.appendChild(anchor);
+        anchor.appendChild(img);
+        list.appendChild(listitems);
+    }
