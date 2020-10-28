94ae30f5097d79c9c0ed42f8265a03bb2efd4c64
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-10-27 20:57:58 -0400

    10-27

--- week7/main.js
+++ week7/main.js
@@ -4,7 +4,7 @@ const apiButton = document.getElementById('chuck');
 
 const outputDiv = document.getElementById('output');
 
-const testURL = 'http://numbersai.com/random';
+const textURL = 'http://numbersai.com/random';
 
 const apiURL = 'http://api.chucknorris.io/jokes/random';
 
@@ -32,6 +32,6 @@ apiButton.addEventListener('click', () => {
         throw Error(response.statusText);
     })
     .then( response => response.json() )
-    .then( data => outputDiv.innerText = data.vale )
+    .then( data => outputDiv.innerText = data.valuee )
     .catch (error => console.log ('There was an error:', error))
 }, false);
