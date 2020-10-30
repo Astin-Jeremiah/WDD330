2d09d43b3519ec2747e47321a59e4d64ffbd2730
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-10-29 23:07:06 -0400

    10-29

---
+++ js/week7.js
@@ -0,0 +1,27 @@
+const links =[
+    { image: "../images/functions2.png", url: "morefunctionsnotes.html"},
+    { image: "../images/ajaxnotes.png", url: "ajaxnotes.html"},
+    { image: "../images/ajax.png", url: "chucknorris.html"},
+    { image: "../images/home.png", url: "https://astin-jeremiah.github.io/WDD330/"},
+]
+
+
+const list = document.getElementById("week7");
+
+for (let i=0; i<links.length; i++)
+    {
+        let listitems = document.createElement("li");
+        let anchor = document.createElement("a");
+        let img = document.createElement("img");
+
+        
+        anchor.setAttribute("href", links[i].url);
+        img.setAttribute("src", links[i].image);
+        img.setAttribute("class", "pressed");
+
+        
+        
+        listitems.appendChild(anchor);
+        anchor.appendChild(img);
+        list.appendChild(listitems);
+    }
--- week7/main.js
+++ week7/main.js
@@ -1,13 +1,17 @@
-const apiButton = document.getElementById('chuck');
+const apichuck = document.getElementById('chuck');
 
-const outputDiv = document.getElementById('output');
+const apiron = document.getElementById('ron');
 
-const apiURL = 'https://api.chucknorris.io/jokes/random';
+const outputDiv = document.getElementById('quoteoutput');
+
+const chuckURL = 'https://api.chucknorris.io/jokes/random';
+
+const ronURL = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes'
 
 
 
-apiButton.addEventListener('click', () => {
-    fetch(apiURL)
+apichuck.addEventListener('click', () => {
+    fetch(chuckURL)
     .then( response => {
         outputDiv.innerHTML = 'Waiting for response...';
         if (response.ok) {
@@ -17,5 +21,19 @@ apiButton.addEventListener('click', () => {
     })
     .then( response => response.json() )
     .then( data => outputDiv.innerText = data.value )
+    .catch (error => console.log ('There was an error:', error))
+}, false);
+
+apiron.addEventListener('click', () => {
+    fetch(ronURL)
+    .then( response => {
+        outputDiv.innerHTML = 'Waiting for response...';
+        if (response.ok) {
+            return response;
+        }
+        throw Error(response.statusText);
+    })
+    .then( response => response.json() )
+    .then( data => outputDiv.innerHTML = `"${data[0]}"` )
     .catch (error => console.log ('There was an error:', error))
 }, false);
