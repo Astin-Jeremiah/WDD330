368baef1656ff477b5092231bda37d10fd2a43d5
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-11-04 22:58:30 -0500

    11-4

---
+++ js/week8.js
@@ -0,0 +1,30 @@
+const links =[
+    { image: "../images/tt.png", url: "transformsnotes.html"},
+    { image: "../images/csd.png", url: "canvasnotes.html"},
+    { image: "../images/transform.png", url: "transforms.html"},
+    { image: "../images/canvas.png", url: "canvasdraw.html"},
+    { image: "../images/SVG.png", url: "svgdraw.html"},
+    { image: "../images/caticon.png", url: "cathat.html"},
+    { image: "../images/home.png", url: "https://astin-jeremiah.github.io/WDD330/"},
+]
+
+
+const list = document.getElementById("week8");
+
+for (let i=0; i<links.length; i++)
+    {
+        let listitems = document.createElement("li");
+        let anchor = document.createElement("a");
+        let img = document.createElement("img");
+
+        
+        anchor.setAttribute("href", links[i].url);
+        img.setAttribute("src", links[i].image);
+        img.setAttribute("class", "pressed");
+
+        
+        
+        listitems.appendChild(anchor);
+        anchor.appendChild(img);
+        list.appendChild(listitems);
+    }
