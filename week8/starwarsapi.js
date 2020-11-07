f2ecd844cde49d74ae49e287def66f8c373fdf71
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-11-07 09:58:35 -0500

    11-7

--- week8/starwarsapi.js
+++ week8/starwarsapi.js
@@ -18,7 +18,7 @@ function loaddata() {
     .then((data) => {
         list.push(...data.results);
         for (let i=0; i < list.length; i++){
-            let summary = `<h2 class="sw2" id=${list[i].url} onclick="loadprofile(this.id)">${list[i].name}</h2>
+            let summary = `<h2 class="sw2" id=${[i]} onclick="loadprofile(this.id, list)">${list[i].name}</h2>
                                 <ul class="wars">
                                 <li class="star">Height: ${list[i].height}</li>
                                 <li class="star">Birth Year: ${list[i].birth_year}</li>
@@ -29,36 +29,26 @@ function loaddata() {
     .catch (error => console.log ('There was an error:', error))
     }
 
-function loadprofile(clicked_id) {
-    outputDiv.className="hidden";
-    output2.className="show";
-    up.className="hidden";
-    down.className="hidden";
-    let url = clicked_id;
-    if(url.match('^http://')){
-     url = url.replace("http://","https://")
-    }
-    output2.innerHTML="";
-    fetch(url)
-    .then((response) => {
-        return response.json();})
-    .then((data) => {
-        profile = data;
-        let fullprofile = `<h2 class="sw3">${profile.name}</h2>
+function loadprofile(clicked_id, list) {
+            outputDiv.className="hidden";
+            output2.className="show";
+            up.className="hidden";
+            down.className="hidden";
+            let item = clicked_id;
+            let fullprofile = `<h2 class="sw3">${list[item].name}</h2>
                                 <ul class="wars">
-                                <li class="star2">Height: ${profile.height}</li>
-                                <li class="star2">Mass: ${profile.mass}</li>
-                                <li class="star2">Hair Color: ${profile.hair_color}</li>
-                                <li class="star2">Skin Color: ${profile.skin_color}</li>
-                                <li class="star2">Eye Color: ${profile.eye_color}</li>
-                                <li class="star2">Birth Year: ${profile.birth_year}</li>
-                                <li class="star2">Gender: ${profile.gender}</li>
+                                <li class="star2">Height: ${list[item].height}</li>
+                                <li class="star2">Mass: ${list[item].mass}</li>
+                                <li class="star2">Hair Color: ${list[item].hair_color}</li>
+                                <li class="star2">Skin Color: ${list[item].skin_color}</li>
+                                <li class="star2">Eye Color: ${list[item].eye_color}</li>
+                                <li class="star2">Birth Year: ${list[item].birth_year}</li>
+                                <li class="star2">Gender: ${list[item].gender}</li>
                                 </ul>
                                 <figure onclick="back()"><img src="../images/fight.png"><figcaption class="regular">Back</figcaption></figure>`
-        output2.innerHTML = fullprofile;
-        })
-    .catch (error => console.log ('There was an error:', error))
-}
+            output2.innerHTML = fullprofile;
+        }
+
 
 function back() {
     outputDiv.className="show";
