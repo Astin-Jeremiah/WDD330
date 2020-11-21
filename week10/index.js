3a416c5215819057d0d8a8f0e4d13abcd8d38191
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-11-20 20:11:56 -0500

    11-20

--- week10/index.js
+++ week10/index.js
@@ -1,4 +1,6 @@
-import QuakesController from './quakescontroller.js';
+function complete() {
+    import QuakesController from './quakescontroller.js';
 
 const myQuakesController = new QuakesController('#quakeList');
 myQuakesController.init();
+}
