be8b68e5639f5f1b2b3e67fec683199644b1863d
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-11-21 00:22:20 -0500

    l

--- week10/index.js
+++ week10/index.js
@@ -2,11 +2,10 @@ import QuakesController from './quakescontroller.js';
 
 const myQuakesController = new QuakesController('#quakeList');
 
-const r = document.getElementById("rad").value;
-const radius = parseInt(r);
-console.log(radius);
-
 document.getElementById("start").addEventListener("click", function() {
+    const r = document.getElementById("rad").value;
+    const radius = parseInt(r);
+    console.log(radius);
     myQuakesController.init(radius);
 });
 
