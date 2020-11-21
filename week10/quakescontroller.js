cda126d207cec5b66b16c0548539ba5c0987671b
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-11-20 23:55:47 -0500

    g

--- week10/quakescontroller.js
+++ week10/quakescontroller.js
@@ -2,7 +2,7 @@ import { getLocation } from './utilities.js';
 import Quake from './quake.js';
 import QuakesView from './quakesview.js';
 
-const r = = document.getElementById("rad").value;
+const r = document.getElementById("rad").value;
 const radius = parseInt(r);
 console.log(radius);
 
