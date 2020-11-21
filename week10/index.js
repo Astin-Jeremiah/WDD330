46d78e7be2a0bae59202cbce004b147957ce5fb0
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-11-21 00:30:49 -0500

    11-20

--- week10/index.js
+++ week10/index.js
@@ -6,6 +6,8 @@ document.getElementById("start").addEventListener("click", function() {
     const r = document.getElementById("rad").value;
     const radius = parseInt(r);
     console.log(radius);
+    const date1 = document.getElementById("D1").value;
+    console.log(date1);
     myQuakesController.init(radius);
 });
 
