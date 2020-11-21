a9909c1febb9d0011c70ddfc3683cadbaded9d04
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-11-20 22:52:32 -0500

    this

--- week10/quakescontroller.js
+++ week10/quakescontroller.js
@@ -48,7 +48,7 @@ export default class QuakesController {
     this.parentElement.innerHTML = '<li>Loading...</li>';
     // get the list of quakes in the specified radius of the location
     const quakeList = await this.quakes.getEarthQuakesByRadius(
-      this.position, radius
+      this.position, this
     );
       console.log(radius);
 
