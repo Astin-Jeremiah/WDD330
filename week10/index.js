c7f2309ebf7859d17eb11d11948122135492a6ca
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-11-20 21:51:12 -0500

    11-20

--- week10/index.js
+++ week10/index.js
@@ -1,3 +1,8 @@
 import QuakesController from './quakescontroller.js';
 
 const myQuakesController = new QuakesController('#quakeList');
+
+document.getElementById("start").addEventListener("click", funcion() {
+    myQuakesController.init();
+});
+
