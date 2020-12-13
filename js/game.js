6a561552e7deb49c5b782573ee6b9d9f44b2226f
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2020-12-12 22:05:06 -0500

    12-12

--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+import categories from '../js/cat.js';
+
 const questionbox = document.querySelector('#questioncard h3');
 const A1 = document.getElementById("a1");
 const B1 = document.getElementById("b1");
@@ -38,8 +40,6 @@ window.addEventListener('load', () => {
   });
    
 });
-
-const categories = [{"id":9,"name":"General Knowledge"},{"id":10,"name":"Entertainment: Books"},{"id":11,"name":"Entertainment: Film"},{"id":12,"name":"Entertainment: Music"},{"id":13,"name":"Entertainment: Musicals & Theatres"},{"id":14,"name":"Entertainment: Television"},{"id":15,"name":"Entertainment: Video Games"},{"id":16,"name":"Entertainment: Board Games"},{"id":17,"name":"Science & Nature"},{"id":18,"name":"Science: Computers"},{"id":19,"name":"Science: Mathematics"},{"id":20,"name":"Mythology"},{"id":21,"name":"Sports"},{"id":22,"name":"Geography"},{"id":23,"name":"History"},{"id":24,"name":"Politics"},{"id":25,"name":"Art"},{"id":26,"name":"Celebrities"},{"id":27,"name":"Animals"},{"id":28,"name":"Vehicles"},{"id":29,"name":"Entertainment: Comics"},{"id":30,"name":"Science: Gadgets"}];
 
 console.log(categories);
 
